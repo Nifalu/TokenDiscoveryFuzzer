f6853b95f698dde81e336cd5ce5ee3c7ffbe2234
Ubuntu <device-admin@tokenfuzzer.fhnw.ch>
2025-11-08 18:22:23 +0000

    unify the different lib fuzzers into a single cargo

---
+++ libfuzzer_libarchive/build_config.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+LIBARCHIVE_VERSION="3.8.2"
+LIBARCHIVE_DIR="libarchive-${LIBARCHIVE_VERSION}"
+
+build_library() {
+    if [ ! -f "libarchive.a" ]; then
+        echo "Building libarchive..."
+        wget https://github.com/libarchive/libarchive/releases/download/v${LIBARCHIVE_VERSION}/libarchive-${LIBARCHIVE_VERSION}.tar.gz
+        tar -xzf libarchive-${LIBARCHIVE_VERSION}.tar.gz
+        cd ${LIBARCHIVE_DIR}
+        cmake -DBUILD_SHARED_LIBS=OFF \
+              -DENABLE_TEST=OFF \
+              -DCMAKE_C_COMPILER="$LIBAFL_CC" \
+              -DCMAKE_CXX_COMPILER="$LIBAFL_CXX" \
+              -G "Unix Makefiles" .
+        make -j$(nproc)
+        cp libarchive/libarchive.a ..
+        cd ..
+    fi
+}
+
+compile_with_token_discovery() {
+    $LIBAFL_CXX_WITH_TOKENS harness.cc libarchive.a \
+        -I ${LIBARCHIVE_DIR}/libarchive/ \
+        -lz -lbz2 -llzma -lzstd -lcrypto -lxml2 \
+        -o libarchive_with_token_discovery
+}
+
+compile_without_token_discovery() {
+    $LIBAFL_CXX_WITHOUT_TOKENS harness.cc libarchive.a \
+        -I ${LIBARCHIVE_DIR}/libarchive/ \
+        -lz -lbz2 -llzma -lzstd -lcrypto -lxml2 \
+        -o libarchive_without_token_discovery
+}
+
+compile_test() {
+    clang++ -DSTANDALONE_BUILD harness.cc libarchive.a \
+        -I ${LIBARCHIVE_DIR}/libarchive/ \
+        -lz -lbz2 -llzma -lzstd -lcrypto -lxml2 \
+        -g -fsanitize=address \
+        -o test_libarchive
+}
+
+clean_target() {
+    echo "Cleaning libarchive build artifacts..."
+    rm -f libarchive_with_token_discovery libarchive_without_token_discovery test_libarchive *.a
+    rm -rf libarchive-${LIBARCHIVE_VERSION}* *.tar.gz
+}
---
+++ libfuzzer_libpng/build_config.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Build configuration for libpng
+
+build_library() {
+    if [ ! -f "libpng16.a" ]; then
+        echo "Building libpng..."
+        wget https://github.com/glennrp/libpng/archive/refs/tags/v1.6.37.tar.gz
+        tar -xzf v1.6.37.tar.gz
+        cd libpng-1.6.37
+        ./configure --enable-shared=no --with-pic=yes
+        make CC="$LIBAFL_CC" CXX="$LIBAFL_CXX" -j$(nproc)
+        cp .libs/libpng16.a ..
+        cd ..
+    fi
+}
+
+compile_fuzzer() {
+    $LIBAFL_CXX harness.cc libpng16.a \
+        -I libpng-1.6.37/ \
+        -DLIBPNG_SILENCE_ERRORS \
+        -lz -lm \
+        -o fuzzer_libpng
+}
+
+compile_test() {
+    clang++ harness.cc libpng16.a \
+        -I libpng-1.6.37/ \
+        -DLIBPNG_SILENCE_ERRORS \
+        -lz -lm \
+        -g -fsanitize=address \
+        -o test_libpng
+}
+
+clean_target() {
+    echo "Cleaning libpng build artifacts..."
+    rm -f fuzzer_libpng test_libpng *.a
+    rm -rf libpng-1.6.37* *.tar.gz
+}
